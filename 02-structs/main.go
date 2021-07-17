eee9ff1e98f95acf290043d4e1b6376f5a52cf94
Luiz Paro <luiz@kiwime.io>
2021-07-17 15:43:23 -0400

    using structs with receiver functions

--- 02-structs/main.go
+++ 02-structs/main.go
@@ -10,18 +10,27 @@ type contactInfo struct {
 type person struct {
 	firstName string
 	lastName  string
-	contact   contactInfo
+	contactInfo
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+func (p person) updateName(newFirstname string) {
+	p.firstName = newFirstname
 }
 
 func main() {
 	jim := person{
 		firstName: "Jim",
 		lastName:  "Party",
-		contact: contactInfo{
+		contactInfo: contactInfo{
 			email:   "jim@gmail.com",
 			zipCode: 9400,
 		},
 	}
 
-	fmt.Printf("%+v", jim)
+	jim.updateName("jimmy")
+	jim.print()
 }
