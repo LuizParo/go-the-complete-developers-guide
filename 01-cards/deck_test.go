19981061125ee5ecac11e6e92b5f43d1ad66054b
Luiz Paro <luiz@kiwime.io>
2021-07-17 00:23:37 -0400

    adding more tests

--- 01-cards/deck_test.go
+++ 01-cards/deck_test.go
@@ -1,12 +1,15 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
 	if len(d) != 16 {
-		t.Errorf("Expected deck length of 20, but got %v", len(d))
+		t.Errorf("Expected deck length of 16, but got %v", len(d))
 	}
 
 	if d[0] != "Ace of Spades" {
@@ -16,4 +19,19 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "Four of Clubs" {
 		t.Errorf("Expected last card of Four of Clubs, but got %v", d[len(d)-1])
 	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
