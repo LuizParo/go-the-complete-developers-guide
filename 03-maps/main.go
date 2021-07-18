f9a712b9fe75cded166b39bcf78130f474403ff8
Luiz Paro <luiz@kiwime.io>
2021-07-18 13:42:16 -0400

    iterating over maps

--- 03-maps/main.go
+++ 03-maps/main.go
@@ -2,19 +2,18 @@ package main
 
 import "fmt"
 
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
+}
+
 func main() {
-	// var colors map[string]string
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
 
-	colors := make(map[string]string)
-
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#4bf745",
-	// }
-
-	colors["white"] = "#ffffff"
-
-	delete(colors, "white")
-
-	fmt.Println(colors)
+	printMap(colors)
 }
