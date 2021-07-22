b8c0157fea02754cffd9a46ad1d379216c690c80
Luiz Paro <luiz@kiwime.io>
2021-07-21 23:49:38 -0400

    creating custom writer

--- 05-http/main.go
+++ 05-http/main.go
@@ -7,6 +7,15 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+
+	return len(bs), nil
+}
+
 func main() {
 	resp, err := http.Get("http://google.com")
 
@@ -15,5 +24,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	io.Copy(os.Stdout, resp.Body)
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
 }
