f7651c23b55b77a9fb1b657bf9c9bdfa7395ed80
Luiz Paro <luiz@kiwime.io>
2021-07-23 23:32:52 -0400

    adding function to check a site status

--- 06-channels/main.go
+++ 06-channels/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+
+	if err != nil {
+		fmt.Println(link, "might be down")
+		return
+	}
+
+	fmt.Println(link, "is up!")
+}
+
 func main() {
 	links := []string{
 		"http://google.com",
@@ -10,6 +26,6 @@ func main() {
 	}
 
 	for _, link := range links {
-
+		checkLink(link)
 	}
 }
